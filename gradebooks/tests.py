65576d4ffa3c0f97005d32719fb055f178d11fbf
benya <rungsri.benya@gmail.com>
2018-12-21 08:03:33 +0700

    tried to write test

--- gradebooks/tests.py
+++ gradebooks/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Learner
+
+
+class LearnerModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Learner.objects.create(name='minimooc1', email='minimooc1@minimooc.poc', grade=1.0)
+
+    def test_name_content(self):
+        learner = Learner.objects.get(id=1)
+        expected_object_name = f'{learner.name}'
+        self.assertEquals(expected_object_name, 'minimooc1')
+
+    def test_email_content(self):
+        learner = Learner.objects.get(id=1)
+        expected_object_name = f'{learner.email}'
+        self.assertEquals(expected_object_name, 'minimooc1@minimooc.poc')
+
+    def test_grade_content(self):
+        learner = Learner.objects.get(id=1)
+        expected_object_name = f'{learner.grade}'
+        self.assertEquals(expected_object_name, '1.0')
