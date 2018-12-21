1335ca52ec655e7d8b6517419f816b58e352ac24
mamilkew <mui_milk@hotmail.com>
2018-12-21 08:17:19 +0700

    created api for trying the react calling

--- gradebooks/urls.py
+++ gradebooks/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('api_learner/', views.api_learner, name='api_learner')
 ]
--- gradebooks/views.py
+++ gradebooks/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from .models import Learner
+from django.http import JsonResponse
 
 # Create your views here.
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
 def index(request):
+    # return render(request, 'index.html', learner)
     return render(request, 'index.html')
     # return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def api_learner(request):
+    learner = Learner.objects.all().values()
+    print(list(learner))
+
+    return JsonResponse({'results': list(learner)})
