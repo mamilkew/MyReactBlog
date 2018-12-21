1e3dcc260cbff4d267d7b4e83bef7a1ec4076979
benya <rungsri.benya@gmail.com>
2018-12-21 08:01:54 +0700

    'Learner' table in admin

--- gradebooks/models.py
+++ gradebooks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Learner(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    grade = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """A string representation of the model."""
+        return self.name
