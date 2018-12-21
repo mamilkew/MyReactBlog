3813dacd194ae814dad1e04322acc3990bd400d7
mamilkew <mui_milk@hotmail.com>
2018-12-21 08:16:30 +0700

    play initial react

--- gradebooks/static/js/App.js
+++ gradebooks/static/js/App.js
@@ -1,34 +1,73 @@
 import React, { Component } from 'react';
 import logo from '../media/logo.svg';
 import '../css/App.css';
-import HelloApp from './HelloApp';
-import MyStatelessComponent from './MyStatelessComponent';
+// import HelloApp from './HelloApp';
+// import MyStatelessComponent from './MyStatelessComponent';
+
+const list = [
+  {
+    'id': 1,
+    'title': '1st Item',
+    'description': 'Description here.'
+  },
+  {
+    'id': 2,
+    'title': '2nd Item',
+    'description': 'Another description here.'
+  },
+  {
+    'id': 3,
+    'title': '3rd Item',
+    'description': 'Third description here.'
+  }
+];
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { list };
+//   }
+//
+//   render() {
+//     return (
+//       <div>
+//         {this.state.list.map(item => (
+//           <div>
+//             <h1>{item.title}</h1>
+//             <span>{item.description}</span>
+//           </div>
+//         ))}
+//       </div>
+//     );
+//   }
+// }
 
 class App extends Component {
+  state = {
+    todos: []
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('http://127.0.0.1:8000/gradebooks/api_learner/');
+      const todos = await res.json();
+      this.setState({
+        todos
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <HelloApp message="This is message sent from App.js"/>
-
-         <MyStatelessComponent
-            title="Stateless"
-            message="Example of Stateless Component"
-          />
-
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {this.state.todos.map(item => (
+          <div key={item.grade}>
+            <h1>{item.name}</h1>
+            <span>{item.email}</span>
+          </div>
+        ))}
       </div>
     );
   }
