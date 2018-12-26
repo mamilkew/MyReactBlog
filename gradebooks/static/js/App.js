49387dc52f24a63ac640ba7ab9f4e6aececff50a
mamilkew <mui_milk@hotmail.com>
2018-12-26 08:35:46 +0700

    combined multiple js file to render in one page

--- gradebooks/static/js/App.js
+++ gradebooks/static/js/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import logo from '../media/logo.svg';
 import '../css/App.css';
 import HelloApp from './HelloApp';
-import MyStatelessComponent from './MyStatelessComponent';
+// import MyStatelessComponent from './MyStatelessComponent';
+import Table_app from './Table';
 // import { LocaleProvider, DatePicker, message } from 'antd';
 // The default locale is en-US, but we can change it to other language
 // import frFR from 'antd/lib/locale-provider/th_TH';
@@ -16,28 +17,30 @@ class App extends Component {
     return (
       <div className="App">
 
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        // <header className="App-header">
+        //   <img src={logo} className="App-logo" alt="logo" />
+        //   <p>
+        //     Edit <code>src/App.js</code> and save to reload.
+        //   </p>
+        //   <a
+        //     className="App-link"
+        //     href="https://reactjs.org"
+        //     target="_blank"
+        //     rel="noopener noreferrer"
+        //   >
+        //     Learn React
+        //   </a>
+        // </header>
 
 
         <HelloApp message="This is message sent from App.js"/>
 
-         <MyStatelessComponent
-            title="Stateless"
-            message="Example of Stateless Component"
-          />
+        // <MyStatelessComponent
+        //   title="Stateless"
+        //   message="Example of Stateless Component"
+        // />
+
+        <Table_app />
       </div>
     );
   }
